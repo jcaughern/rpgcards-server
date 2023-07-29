362764dbb9f44d3a07a9e77d3fd9cae94b6f647f
Jake Caughern <80003881+jcaughern@users.noreply.github.com>
2023-07-29 14:35:21 -0500

    added Spring Initializr project

---
+++ src/main/java/com/caughern/rpgcardsserver/RpgcardsServerApplication.java
@@ -0,0 +1,13 @@
+package com.caughern.rpgcardsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgcardsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgcardsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caughern/rpgcardsserver/RpgcardsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caughern.rpgcardsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgcardsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
